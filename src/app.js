36b502528f377bbdb4ae3d7cbf1b4a1aa8c3ec67
KatieRiches <103969925+KatieRiches@users.noreply.github.com>
2022-05-24 13:38:51 +1200

    Integrated forecast api

--- src/app.js
+++ src/app.js
@@ -21,7 +21,8 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
 
   let days = ["Thu", "Fri", "Sat", "Sun"];
@@ -51,6 +52,13 @@ function displayForecast() {
   console.log(forecastHTML);
 }
 
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "4cf67821f8a1ed58161e24f9867203f3";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function displayTemperture(response) {
   let city = document.querySelector("#city");
   let temperature = document.querySelector("#temperature");
@@ -73,6 +81,8 @@ function displayTemperture(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   icon.setAttribute("alt", response.data.weather[0].description);
+
+  get.forecast(response.data.coord);
 }
 
 function search(city) {
@@ -117,4 +127,3 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("london");
-displayForecast();
